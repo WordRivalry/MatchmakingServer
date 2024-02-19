324a49603a36f12b27250d719c46ae05aec3fa1e
exgael <benoitbarbier.dev@gmail.com>
2024-02-18 23:41:46 -0500

    rm comments

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,14 @@ import { ConnectionManager } from './ConnectionManager';
 import { MatchFoundService } from "./Services/MatchFoundService";
 import { WebSocketEventHandler } from "./Services/WebSocketEventHandler";
 
-// Instantiate the PlayerSessionStore
 const playerSessionStore: PlayerSessionStore = new PlayerSessionStore();
-
-// Instantiate the CommunicationService
 const matchFoundService: MatchFoundService = new MatchFoundService(playerSessionStore);
-
-// Instantiate the MatchmakingQueue with the PlayerSessionStore
 const matchmakingService: MatchmakingService = new MatchmakingService();
-
-// Instantiate the MessageHandler with the MatchmakingQueue and PlayerSessionStore
-const messageHandler: WebSocketEventHandler = new WebSocketEventHandler(matchmakingService, playerSessionStore, matchFoundService);
-
-// Instantiate the ConnectionManager with the MatchmakingQueue and PlayerSessionStore
+const messageHandler: WebSocketEventHandler = new WebSocketEventHandler(
+    matchmakingService, 
+    playerSessionStore, 
+    matchFoundService
+);
 const connectionManager: ConnectionManager = new ConnectionManager(messageHandler);
 
 // Start the server
